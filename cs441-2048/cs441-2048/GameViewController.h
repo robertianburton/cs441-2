88e27822dc3411358191558156f3f73eda8a4b88
John Doe <rburton2@binghamton.edu>
2019-02-13 02:30:01 -0500

    Prepared some of grid

--- cs441-2048/cs441-2048/GameViewController.h
+++ cs441-2048/cs441-2048/GameViewController.h
@@ -12,7 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GameViewController : UIViewController
 @property (nonatomic, strong) IBOutlet UILabel *statusLabel;
-@property UIGestureRecognizer;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeLeft;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeRight;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeUp;
+@property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeDown;
 @end
 
 NS_ASSUME_NONNULL_END
--- cs441-2048/cs441-2048/GameViewController.m
+++ cs441-2048/cs441-2048/GameViewController.m
@@ -13,12 +13,41 @@
 @end
 
 @implementation GameViewController
-@synthesize statusLabel;
+@synthesize statusLabel, swipeLeft, swipeRight, swipeUp, swipeDown;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+
+    swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeAction:)];
+    [swipeLeft setDirection:UISwipeGestureRecognizerDirectionLeft];
+    [self.view addGestureRecognizer:swipeLeft];
+    swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeAction:)];
+    [swipeRight setDirection:UISwipeGestureRecognizerDirectionRight];
+    [self.view addGestureRecognizer:swipeRight];
+    swipeUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeAction:)];
+    [swipeUp setDirection:UISwipeGestureRecognizerDirectionUp];
+    [self.view addGestureRecognizer:swipeUp];
+    swipeDown = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeAction:)];
+    [swipeDown setDirection:UISwipeGestureRecognizerDirectionDown];
+    [self.view addGestureRecognizer:swipeDown];
+    
     [statusLabel setText:@"Testing!"];
+}
+- (IBAction)swipeAction:(UISwipeGestureRecognizer *)sender {
+    if (sender.direction == UISwipeGestureRecognizerDirectionLeft) {
+        NSLog(@"Left Swipe");
+    }
+    if (sender.direction == UISwipeGestureRecognizerDirectionRight) {
+        NSLog(@"Right Swipe");
+    }
+    if (sender.direction == UISwipeGestureRecognizerDirectionUp) {
+        NSLog(@"Up Swipe");
+    }
+    if (sender.direction == UISwipeGestureRecognizerDirectionDown) {
+        NSLog(@"Down Swipe");
+    }
+    [statusLabel setText:@"Swipe!"];
 }
 
 /*
